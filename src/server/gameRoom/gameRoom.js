51d8b0fba995a87e25a6e964e565e6cee549ac13
dorgrin6 <dorgrin6@gmail.com>
2018-07-28 16:36:27 +0300

    Added gameRoomMangement server

--- src/server/gameRoom/gameRoom.js
+++ src/server/gameRoom/gameRoom.js
@@ -1,15 +1,29 @@
 const auth = require("../auth");
 const games = require("../games");
 
-function getBoardState() {
+function getBoardState(req, res, next) {
   const gameName = auth.getUserInfo(req.session.id).gameName;
   const game = games.getGameByName(gameName);
-  const gameLogic = game.gameLogic;
 
-  return game.gameLogic;
+  res.send(game.gameLogic);
+  next();
 }
 
+function playCard(req, res, next) {
+  const player = auth.getUserInfo(req.session.id);
+  const gameName = player.gameName;
+  const game = games.getGameByName(gameName);
+
+  const gameLogic = game.gameLogic;
+
+  gameLogic.drawCardsWhenNoLegal(player);
+  next();
+}
+
+function drawCard(req, res, next) {}
 
 module.exports = {
-  getBoardState
+  getBoardState,
+  playCard,
+  drawCard
 };
--- src/server/gameRoom/gameRoomMangement.js
+++ src/server/gameRoom/gameRoomMangement.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const gameRoom = require("./gameRoom");
+const auth = require("../auth");
 
 const gameRoomManagement = express.Router();
 
-gameRoomManagement.get("/boardState", gameRoom.getBoardState, (req, res) => {
+gameRoomManagement.get("/boardState", auth.userAuthentication, gameRoom.getBoardState, (req, res) => {
   res.sendStatus(200);
 });
+
+gameRoomManagement.post("/drawCard", auth.userAuthentication, gameRoom.drawCard, (req, res) => {
+  res.sendStatus(200);
+});
+
+gameRoomManagement.post("/playCard", auth.userAuthentication, gameRoom.playCard, (req, res) => {
+  res.sendStatus(200);
+});
+
 
 module.exports = gameRoomManagement;
